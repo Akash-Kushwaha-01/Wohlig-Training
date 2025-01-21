ed812a08237d836e62a710c4e65985f18955fe81
Akash-Kushwaha-01 <akash.kushwaha@wohlig.com>
2025-01-21 13:04:07 +0530

    Create Schema Person for Hotel Management System

---
+++ MondoDB_Basics_CRUD/models/person.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+// Define a Mongoose Schema
+const personSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    age: {
+        type: Number,
+    },
+    work: {
+        type: String,
+        enum: ['chef', 'waiter', 'manager']
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    address: {
+        type: String,
+    },
+    salary: {
+        type: Number,
+        required: true
+    }
+});
+
+// Create Person Model
+const Person = mongoose.model('Person', personSchema);
+
+// Export the model
+module.exports = Person;
